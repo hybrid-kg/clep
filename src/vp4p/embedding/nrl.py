4fb52a3189bb8e18a2872dea3446acacc43f88fb
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-10-25 13:50:53 +0200

    Added better docstring and comments

--- src/vp4p/embedding/nrl.py
+++ src/vp4p/embedding/nrl.py
@@ -3,17 +3,21 @@
 """Embed the gene data & pathway using Network Representation Learning."""
 
 import pickle
-from typing import Any, TextIO
+from typing import TextIO, Dict, Tuple
 
 import numpy as np
 import pandas as pd
 from bionev.embed_train import embedding_training
 
 
-# TODO: Implement BioNEV
-
 def do_nrl(data: pd.DataFrame, kg_data: pd.DataFrame, out, method) -> None:
-    """Carry out Network-Representation Learning for the given pandas dataframe."""
+    """Carry out Network-Representation Learning for the given pandas dataframe.
+
+    :param data: Dataframe containing the data to be embedded
+    :param kg_data: Dataframe containing the knowledge graph
+    :param out: Path to the output directory
+    :param method: model that should be used for creating the embedding
+    """
 
     # Get labels
     label = data['label']
@@ -27,42 +31,52 @@ def do_nrl(data: pd.DataFrame, kg_data: pd.DataFrame, out, method) -> None:
 
         kg_mapping = _make_kg_edgelist(kg_data, joint_mapping, data_edge)
 
-    # Pickle patient, gene & Knowledge Graph to numerical representation dictionaries
+    # Pickle patient, gene & knowledge graph to numerical representation dictionaries
     with open(f'{out}/mapping.pkl', 'wb') as mapping_file:
         mapping = {**joint_mapping, **kg_mapping}
         pickle.dump(mapping, mapping_file)
 
+    # Generate embeddings using the model
     embeddings = _gen_embedding(input_path=f'{out}/data.edgelist',
                                 method=method,
                                 model_out=f'{out}/model.gz',
-                                embeddings_out=f'{out}/raw.embedding')
+                                embeddings_out=f'{out}/raw.embedding',
+                                word2vec_model_out=f'{out}/word2vec.pkl')
 
+    # Initialize lists for output dataframe
     label_col = list()
     vectors = list()
     index = list()
+
+    # Create inverse of the sample mapping
     inv_pat_map = {v: k for k, v in pat_mapping.items()}
 
+    # Loop over the embeddings to find the sample nodes
     for node in embeddings.keys():
         if int(node) in label_mapping.keys():
             label_col.append(label_mapping[int(node)])
             vectors.append(embeddings[node])
             index.append(inv_pat_map[int(node)])
 
+    # Create an output dataframe with the embeddings and labels for the samples
     out_df = pd.DataFrame(index=index, data=vectors)
     out_df['label'] = label_col
 
     out_df.to_csv(f'{out}/embedding.tsv', sep='\t')
 
 
-def _make_data_edgelist(data, label, data_edge):
+def _make_data_edgelist(data, label, data_edge) -> Tuple[Dict[str: int], Dict[str: int], Dict[str: int]]:
     """Create an edgelist for the patient data."""
-
+    # Create a mapping from every samples to an unique node ID for the node representation
     pat_mapping = dict((key, val) for val, key in enumerate(np.unique(data['patients'])))
 
+    # Get the max node ID so as to continue the mapping from that number
     max_val = _get_max_dict_val(pat_mapping)
 
+    # Create a mapping for all the genes starting from the max node ID
     gene_mapping = dict(zip(data.columns[1:], range(max_val + 1, len(data.columns[1:]) + max_val + 1)))
 
+    # Only the single sample expression values that are not either up or down regulated are considered for the embedding
     valid_patients = []
     for patient, gene, value in pd.melt(data, id_vars=['patients']).values:
         if value == 0:
@@ -71,8 +85,8 @@ def _make_data_edgelist(data, label, data_edge):
 
         print(pat_mapping[patient], gene_mapping[gene], sep=' ', file=data_edge)
 
+    # Add the valid samples with their corresponding labels to a mapping
     label_mapping = dict()
-
     for idx in data.index:
         if data.at[idx, 'patients'] in valid_patients:
             label_mapping[pat_mapping[data.at[idx, 'patients']]] = label[idx]
@@ -80,10 +94,15 @@ def _make_data_edgelist(data, label, data_edge):
     return pat_mapping, gene_mapping, label_mapping
 
 
-def _make_kg_edgelist(kg_data, joint_mapping, data_edge: TextIO) -> dict:
+def _make_kg_edgelist(kg_data, joint_mapping, data_edge: TextIO) -> Dict[str: int]:
+    """Create an edgelist for the knowledge graph data."""
     kg_mapping = dict()
+
+    # Get the max node ID for the joint mapping of the samples and the genes
     max_val = _get_max_dict_val(joint_mapping)
 
+    # Loop over the 1st (source) and the 3rd (target) columns to add their values to the knowledge graph mapping or
+    # use the ID from the joint mapping if it already exists after normalization of the values.
     for col in [kg_data.iloc[:, 0], kg_data.iloc[:, 2]]:
         for val_raw in col:
             val = val_raw.split(':')[1] if len(val_raw.split(':')) > 1 else val_raw.split(':')[0]
@@ -95,13 +114,15 @@ def _make_kg_edgelist(kg_data, joint_mapping, data_edge: TextIO) -> dict:
                 max_val += 1
                 kg_mapping[val_raw] = max_val
 
+    # Append the source to target mapping to the main data edgelist
     for idx in kg_data.index:
         print(kg_mapping[kg_data.iat[idx, 0]], kg_mapping[kg_data.iat[idx, 2]], sep=' ', file=data_edge)
 
     return kg_mapping
 
 
-def _get_max_dict_val(dictionary: dict) -> Any:
+def _get_max_dict_val(dictionary: dict) -> int:
+    """Get the maximum value from a key, value pair in a given dictionary."""
     return dictionary[max(dictionary, key=lambda value: dictionary[value])]
 
 
@@ -111,6 +132,7 @@ def _gen_embedding(
         method,
         embeddings_out,
         model_out,
+        word2vec_model_out,
         dimensions: int = 300,
         number_walks: int = 8,
         walk_length: int = 8,
@@ -124,7 +146,7 @@ def _gen_embedding(
         order: int = 3,
         weighted: bool = False,
 ):
-    """Generate a NRL embedding using the model."""
+    """Generate a NRL embedding using the given model."""
     model = embedding_training(
         train_graph_filename=input_path,
         method=method,
@@ -141,12 +163,19 @@ def _gen_embedding(
         order=order,
         weighted=weighted,
     )
-    if model_out is not None:
-        model.save_model(model_out)
+
+    # Save the model
+    model.save_model(model_out)
+
+    # Save the embeddings
     model.save_embeddings(embeddings_out)
+
+    # Get the embeddings
     if method == 'LINE':
         embeddings = model.get_embeddings_train()
     else:
+        # Save the Word2Vec model in-case Deepwalk or Node2Vec was used
+        model.word2vec.save(word2vec_model_out)
         embeddings = model.get_embeddings()
 
     return embeddings
