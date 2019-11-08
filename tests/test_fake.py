25a639fdd85b348407ee80be5e5128eb845c68fd
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2019-11-08 15:38:17 +0100

    Add tests and more conf

---
+++ tests/test_fake.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Tests constants."""
+import unittest
+
+class TestShitty(unittest.TestCase):
+    """Stupid Test."""
+
+    def test_fake(self):
+        """Fake test to test that travis is set up properly."""
+        self.assertEqual('hello', 'hello')
