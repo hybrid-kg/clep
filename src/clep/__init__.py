d8760e55163b901ed7fbc2ca7dfb45b19bc3fbae
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2020-07-29 11:38:21 +0200

    Improve metadata

--- src/clep/__init__.py
+++ src/clep/__init__.py
@@ -1,31 +1,4 @@
 # -*- coding: utf-8 -*-
 
-"""A Python package for generating Hybrid Data- and Knowledge- Driven Patient Representations.."""
+"""A Python package for generating Hybrid Data- and Knowledge- Driven Patient Representations."""
 
-"""
-Installation
-------------
-1. ``clep`` can be installed with the following commands:
-
-.. code-block:: sh
-
-    python3 -m pip install git+FIXME
-
-2. or in editable mode with:
-
-.. code-block:: sh
-
-    git clone FIXME
-
-.. code-block:: sh
-
-    cd clep
-
-.. code-block:: sh
-
-    python3 -m pip install -e .
-
-How to use
-----------
-
-"""
