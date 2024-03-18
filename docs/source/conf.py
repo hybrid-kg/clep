04cf772725a868091b263d51387eb27b84701a63
vinay.srinivas.bharadhwaj <vinay.srinivas.bharadhwaj@scai.fraunhofer.de>
2024-03-18 14:48:27 +0100

    Bump version: 0.03-dev -> 0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ project = 'CLEP'
 copyright = '2019-2020, Vinay Bharadhwaj, Daniel Domingo-Fernández and Charles Tapley Hoyt'
 author = 'Vinay Bharadhwaj, Daniel Domingo-Fernández and Charles Tapley Hoyt'
 
-release = '0.0.3-dev'
+release = '0.0.3'
 
 parsed_version = re.match(
     '(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?:-(?P<release>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+(?P<build>[0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?',
