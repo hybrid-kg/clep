197f112ce8fc2fe299313036b0120ebe5aae199c
vinaysb <vinaysbharadhwaj@gmail.com>
2024-06-25 15:54:18 +0200

    add relative imports

--- src/clep/embedding/__init__.py
+++ src/clep/embedding/__init__.py
@@ -2,8 +2,8 @@
 
 """Vectorization for clep."""
 
-from clep.embedding.evaluate import do_ss_evaluation
-from clep.embedding.kge import do_kge
-from clep.embedding.network_generator import do_graph_gen
+from .evaluate import do_ss_evaluation
+from .kge import do_kge
+from .network_generator import do_graph_gen
 
 __all__ = ['do_ss_evaluation', 'do_kge', 'do_graph_gen']
--- src/clep/sample_scoring/__init__.py
+++ src/clep/sample_scoring/__init__.py
@@ -2,9 +2,9 @@
 
 """Single Sample Scoring for clep."""
 
-from clep.sample_scoring.limma import do_limma
-from clep.sample_scoring.ssgsea import do_ssgsea
-from clep.sample_scoring.z_score import do_z_score
-from clep.sample_scoring.radical_search import do_radical_search
+from .limma import do_limma
+from .ssgsea import do_ssgsea
+from .z_score import do_z_score
+from .radical_search import do_radical_search
 
 __all__ = ['do_limma', 'do_ssgsea', 'do_z_score', 'do_radical_search']
